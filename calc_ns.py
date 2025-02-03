cfb734fe5c729b4d39c363ca4cacb474efeeeb5d
Guilherme Zucatelli <g.zucatelli@Guilhermes-MacBook-Air.local>
2025-02-03 11:20:30 -0300

    black codestyle

--- calc_ns.py
+++ calc_ns.py
@@ -6,44 +6,69 @@ from tqdm.contrib.concurrent import process_map
 
 from estimators.ins import INS
 
-ESTIMATORS = {'INS': INS}
+ESTIMATORS = {"INS": INS}
 AUDIO_SAMPLE_RATES = [8000, 16000, 32000, 441000, 48000, 96000]
 
-def get_args():
-    parser = argparse.ArgumentParser(description="Arguments to run pyNSE (Python Non-Stationary Estimator Toolbox).")
 
-    parser.add_argument('-p', '--path', type=str, help="Path to .WAV signal or .TXT list of WAV files.")
-    parser.add_argument('-e', '--estimator', type=str, default='INS', help=f'Estimator name. Available options are {ESTIMATORS.keys()}')
-    parser.add_argument('-sr', '--sample_rate', type=int, default=16000, help=f'Sample rate. Available options are {ESTIMATORS.keys()}')
-    parser.add_argument('-obs', '--observed_scales', nargs='+', default=[0.2], help='List of observed scales relative to total length, values in (0,1]')
-    parser.add_argument('-ns', '--num_surrogates', type=int, default=50, help="Number of surrogates for INS estimator.")
+def get_args():
+    parser = argparse.ArgumentParser(
+        description="Arguments to run pyNSE (Python Non-Stationary Estimator Toolbox)."
+    )
+
+    parser.add_argument(
+        "-p", "--path", type=str, help="Path to .WAV signal or .TXT list of WAV files."
+    )
+    parser.add_argument(
+        "-e",
+        "--estimator",
+        type=str,
+        default="INS",
+        help=f"Estimator name. Available options are {ESTIMATORS.keys()}",
+    )
+    parser.add_argument(
+        "-sr",
+        "--sample_rate",
+        type=int,
+        default=16000,
+        help=f"Sample rate. Available options are {ESTIMATORS.keys()}",
+    )
+    parser.add_argument(
+        "-obs",
+        "--observed_scales",
+        nargs="+",
+        default=[0.2],
+        help="List of observed scales relative to total length, values in (0,1]",
+    )
+    parser.add_argument(
+        "-ns",
+        "--num_surrogates",
+        type=int,
+        default=50,
+        help="Number of surrogates for INS estimator.",
+    )
 
     return vars(parser.parse_args(args=sys.argv[1:]))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = get_args()
 
-    input_path = args['path']
-    estimator_name = args['estimator']
-    sample_rate = args['sample_rate']
-    scales = np.array([float(obs) for obs in args['observed_scales']])
+    input_path = args["path"]
+    estimator_name = args["estimator"]
+    sample_rate = args["sample_rate"]
+    scales = np.array([float(obs) for obs in args["observed_scales"]])
 
-    if estimator_name == 'INS':
-        num_surrogates = args['num_surrogates']
+    if estimator_name == "INS":
+        num_surrogates = args["num_surrogates"]
         estimator = ESTIMATORS[estimator_name](sample_rate, scales, num_surrogates)
     else:
         pass
 
     _, file_extension = os.path.splitext(input_path)
 
-    if file_extension == '.wav':
+    if file_extension == ".wav":
         estimator(input_path)
     else:
         with open(input_path) as file:
             files = [line.rstrip() for line in file]
         process_map(estimator, files)
-
-
-
-    
--- estimators/aux_modules/audio_handler.py
+++ estimators/aux_modules/audio_handler.py
@@ -4,30 +4,32 @@ import soundfile as sf
 from tqdm.contrib.concurrent import process_map
 import resampy
 
+
 class AudioHandler(object):
-    
+
     @staticmethod
     def read_wav(filepath, sample_rate):
         data, SR = sf.read(filepath)
 
-        if len(data.shape)>1:
-            data = data[:,1]
+        if len(data.shape) > 1:
+            data = data[:, 1]
 
         data_rs = resampy.resample(data, SR, sample_rate)
         return data_rs, sample_rate
-    
+
     @staticmethod
     def soxi_get_length(filepath):
         cmd = f"soxi -D {filepath}"
-        length = float(os.popen(cmd).read().replace("\n",""))
+        length = float(os.popen(cmd).read().replace("\n", ""))
         return (filepath, length)
 
     @staticmethod
     def get_file_lengths(list_wav_files):
         print(f"Lenght-Based Preprocessing:")
-        list_filepath_lengths = process_map(AudioHandler.soxi_get_length,
-                                   list_wav_files)
-        
+        list_filepath_lengths = process_map(
+            AudioHandler.soxi_get_length, list_wav_files
+        )
+
         list_lengths = [args[1] for args in list_filepath_lengths]
         list_lengths = list(set(list_lengths))
         return list_filepath_lengths, list_lengths
@@ -35,6 +37,8 @@ class AudioHandler(object):
     @staticmethod
     def slice_signal(signal, window_size, hop_size):
         start_window = np.arange(window_size)
-        num_windows = int( (len(signal)-window_size) // hop_size + 1)
-        window_indeces = np.array([start_window + idx*hop_size for idx in range(num_windows)])
+        num_windows = int((len(signal) - window_size) // hop_size + 1)
+        window_indeces = np.array(
+            [start_window + idx * hop_size for idx in range(num_windows)]
+        )
         return signal[window_indeces]
